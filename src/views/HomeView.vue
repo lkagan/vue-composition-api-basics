4d50a00ac280c9a4b221016f97ae2e1596a94c2c
Larry Kagan <larry@superiocity.com>
2022-04-06 09:39:21 -0400

    Setup of simple counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <div>
+      <button @click="decreaseCounter" class="btn">-</button>
+      <span class="counter">{{ counter }}</span>
+      <button @click="increaseCounter" class="btn">+</button>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      counter: 0
+    }
+  },
+
+  methods: {
+    increaseCounter () {
+      this.counter++
+    },
+
+    decreaseCounter () {
+      this.counter--
+    }
+  }
+}
+</script>
+<style>
+  .home {
+    padding: 20px;
+    text-align: center;
+  }
+  .home .btn {
+    margin: 0 10px;
+    font-size: 30px;
+  }
+  .home .counter {
+    font-size: 30px;
+  }
+</style>
