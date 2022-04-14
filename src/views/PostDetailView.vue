320af67e5a5790c702931fc6864ac46d2bc11782
Larry Kagan <larry@superiocity.com>
2022-04-14 10:53:58 -0400

    Use router composable

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -7,16 +7,33 @@
     <div>
       <button @click="showPostId">Show Post ID</button>
     </div>
+    <div>
+      <button @click="goHomeIn3Seconds">Go Home in 3 Seconds</button>
+    </div>
+    <div>
+      <button @click="goToFirstPost">Go to first post</button>
+    </div>
     <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
   </div>
 </template>
 <script setup>
 
-import {useRoute} from 'vue-router'
+import {useRoute, useRouter} from 'vue-router'
 
 const route = useRoute();
+const router = useRouter();
 
 const showPostId = () => {
   alert('Post ID: ' + route.params.id);
+}
+
+const goHomeIn3Seconds = () => {
+  setTimeout(() => {
+    router.push({name: 'home'});
+  }, 3000);
+}
+
+const goToFirstPost = () => {
+  router.push({name: 'post-detail', params: {id: 1}});
 }
 </script>
