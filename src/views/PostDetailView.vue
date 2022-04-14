ef0f0403436610b60ee216c3adeb3086668c87be
Larry Kagan <larry@superiocity.com>
2022-04-14 10:46:38 -0400

    Access route parameters

--- src/views/PostDetailView.vue
+++ src/views/PostDetailView.vue
@@ -4,8 +4,19 @@
     <p>
       Display the content of post with ID of {{  $route.params.id }} here.
     </p>
+    <div>
+      <button @click="showPostId">Show Post ID</button>
+    </div>
     <p><RouterLink to="/posts">&lt; Back</RouterLink></p>
   </div>
 </template>
 <script setup>
+
+import {useRoute} from 'vue-router'
+
+const route = useRoute();
+
+const showPostId = () => {
+  alert('Post ID: ' + route.params.id);
+}
 </script>
