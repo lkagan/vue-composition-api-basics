a90922321f0cf9f2105d8b19269a712b2e6bd5fb
Larry Kagan <larry@superiocity.com>
2022-04-14 10:32:39 -0400

    Create a post detail page and links to it

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="posts">
     <h1>Posts</h1>
+    <ul>
+      <li>
+        <RouterLink :to="{ name: 'post-detail', params: { id: 'id1'}}">Post 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'post-detail', params: { id: 'id2'}}">Post 2</RouterLink>
+      </li>
+      <li>
+        <RouterLink :to="{ name: 'post-detail', params: { id: 'id3'}}">Post 3</RouterLink>
+      </li>
+    </ul>
     <textarea v-autofocus
     ></textarea>
   </div>
@@ -12,3 +23,9 @@
  */
 import { vAutofocus } from "@/directives/vAutofocus";
 </script>
+
+<style scoped>
+ul {
+  margin-bottom: 30px;
+}
+</style>
