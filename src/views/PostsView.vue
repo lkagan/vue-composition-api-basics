e137be1437aa0d09381728a839533740615aa27b
Larry Kagan <larry@superiocity.com>
2022-04-15 07:54:08 -0400

    V-for is same in composition API

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -2,14 +2,11 @@
   <div class="posts">
     <h1>Posts</h1>
     <ul>
-      <li>
-        <RouterLink :to="{ name: 'post-detail', params: { id: 'id1'}}">Post 1</RouterLink>
-      </li>
-      <li>
-        <RouterLink :to="{ name: 'post-detail', params: { id: 'id2'}}">Post 2</RouterLink>
-      </li>
-      <li>
-        <RouterLink :to="{ name: 'post-detail', params: { id: 'id3'}}">Post 3</RouterLink>
+      <li
+          v-for="post in posts"
+          key="post.id"
+      >
+        <RouterLink :to="{ name: 'post-detail', params: { id: post.id}}">{{ post.title}}</RouterLink>
       </li>
     </ul>
     <textarea v-autofocus
@@ -22,6 +19,14 @@
  * Imports
  */
 import { vAutofocus } from "@/directives/vAutofocus";
+import { ref } from "vue";
+
+const posts = ref([
+  { id: "id1", title: "Post 1" },
+  { id: "id2", title: "Post 2" },
+  { id: "id3", title: "Post 3" },
+]);
+
 </script>
 
 <style scoped>
